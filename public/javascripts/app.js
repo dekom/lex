87507fcb455d22da14d74d5ac300f67a788fc1e2
Xing Zhou <xing@xingzhou.me>
2012-11-16 10:06:32 -0500

    Create query interface for all relevant Wordnik API

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,19 +1,50 @@
-'use strict'
-angular.module('LeX', ['ngResource']).
-  factory('Wordnik', function($resource) {
-    var baseUrl = 'http://api.wordnik.com/v4'
-      , apiKey = 'd25b766254dd7337441237e86ca0f9460f6037ff7432ae240'
-      , querySize = 5
+var lex = angular.module('LeX', ['ngResource'])
 
-    return  $resource(baseUrl + '/word.json/:word/:action',
-                      {}, {
-                            get:  {
-                                    method: 'JSONP',
-                                    params: {
-                                              api_key: apiKey,
-                                              callback: 'JSON_CALLBACK'
-                                            }
-                                  }
-                          }
-                      )
-  })
+// Create Wordnik interface
+lex.factory('Wordnik', function($resource) {
+  var baseUrl = 'http://api.wordnik.com/v4'
+    , apiKey = 'd25b766254dd7337441237e86ca0f9460f6037ff7432ae240'
+    , querySize = 5
+    , wordnik = {}
+    , params =  { api_key: apiKey
+                , limit: querySize
+                , callback: 'JSON_CALLBACK'
+                }
+
+  // Default list of possible queries and the resource method they need to
+  // call.  The difference between 'get' and 'query' is that 'query' returns
+  // an array, while 'get' returns an object
+  wordnik.actions = { 'examples' : 'get'
+                    , 'definitions' : 'query'
+                    , 'topExample' : 'get'
+                    , 'relatedWords' : 'query'
+                    , 'pronunciations' : 'query'
+                    , 'etymologies' : 'query'
+                    , 'audio' : 'query'
+                    }
+
+  wordnik.q =
+          $resource(
+                    baseUrl + '/word.json/:word/:action'
+                    , {}
+                    , { get:  { method: 'JSONP'
+                              , params: params
+                              }
+                      , query:  { method: 'JSONP'
+                                , params: params
+                                , isArray: true
+                                }
+                      }
+                    )
+  
+  wordnik.parse = { 'definitions' : function() {}
+                  , 'examples' : function() {}
+                  , 'topExample' : function() {}
+                  , 'relatedWords' : function() {}
+                  , 'pronunciations' : function() {}
+                  , 'etymologies' : function() {}
+                  , 'audio' : function() {}
+  }
+
+  return wordnik
+})
--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,11 +1,28 @@
 // Handles query to Wordnik API
 
 function WordnikCtrl($scope, Wordnik) {
+
+  // Default action to 'definitions'
+  $scope.action = 'definitions'
+  $scope.actions = Object.keys(Wordnik.actions)
+
+  // Default query word
+  $scope.query = 'lexicon'
+
   $scope.search = function() {
-    Wordnik.get({action: 'definitions', word: $scope.query}).
-      success(function(data, status) {
-        $scope.result = data
-      })
+    var results =
+        Wordnik.q[Wordnik.actions[$scope.action]](
+                              { action: $scope.action
+                              , word: $scope.query
+                              , useCanonical: true
+                              }
+                              , function success() {
+                                  $scope.results = results
+                                }
+                              , function error() {
+                              
+                              }
+    )
   }
 }
 
